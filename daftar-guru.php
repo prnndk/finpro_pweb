c4c16e21048dc86e30d21a697a3065a17fd47aa1
Muhammad Rifqi Ma'ruf <mrifqim.id@gmail.com>
2023-12-13 11:44:10 +0700

    bug

--- daftar-guru.php
+++ daftar-guru.php
@@ -1,17 +1,6 @@
 <?php
 
-$servername = 'localhost';
-$username = 'root';
-$password = '';
-$database = 'finpro_pweb';
-
-// Create connection
-$connect = new mysqli($servername, $username, $password, $database);
-
-// Check connection
-if ($connect->connect_error) {
-    exit('Connection failed: '.$connect->connect_error);
-}
+include 'connection.php';
 
 // Query untuk mendapatkan data pengajar
 $query = "SELECT * FROM pengajar";
@@ -30,9 +19,6 @@ if ($result === false) {
         echo '<br>';
     }
 }
-
-// Menutup koneksi
-$connect->close();
 
 ?>
 <!DOCTYPE html>
@@ -194,12 +180,71 @@ $connect->close();
         <?php include 'sidebar.php'; ?>
 
         <nav class="navbar nav-primary header" id="header">
-            <!-- Navbar content remains unchanged -->
-        </nav>
-
-        <div class="l-navbar" id="nav-bar">
-            <!-- Sidebar content remains unchanged -->
+        <div class="container-fluid">
+            <div class="header_toggle">
+                <i class="bx bx-menu text-white" id="header-toggle"></i>
+            </div>
+            <div class="d-flex">
+                <div class="dropdown text-end">
+                    <a href="#" class="d-block link-dark text-decoration-none dropdown-toggle" data-bs-toggle="dropdown"
+                        aria-expanded="false">
+                        <img src="https://github.com/mdo.png" alt="mdo" width="40" height="40" class="rounded-circle" />
+                    </a>
+                    <ul class="dropdown-menu text-small">
+                        <li class="dropdown-header">Admin Menu</li>
+                        <li><a class="dropdown-item" href="#">New project...</a></li>
+                        <li><a class="dropdown-item" href="#">Settings</a></li>
+                        <li><a class="dropdown-item" href="#">Profile</a></li>
+                        <li>
+                            <hr class="dropdown-divider" />
+                        </li>
+                        <li><a class="dropdown-item" href="#">Sign out</a></li>
+                    </ul>
+                </div>
+            </div>
         </div>
+    </nav>
+
+    <div class="l-navbar" id="nav-bar">
+        <nav class="nav">
+            <div>
+                <a href="#" class="nav_logo">
+                    <i class="bx bx-layer nav_logo-icon"></i>
+                    <span class="nav_logo-name">ACC BIMBEL</span>
+                </a>
+                <div class="nav_list">
+                    <a href="#" class="nav_link active">
+                        <i class="bx bx-grid-alt nav_icon"></i>
+                        <span class="nav_name">Dashboard</span>
+                    </a>
+                    <a href="#" class="nav_link">
+                        <i class="bx bx-user nav_icon"></i>
+                        <span class="nav_name">Users</span>
+                    </a>
+                    <a href="#" class="nav_link">
+                        <i class="bx bx-message-square-detail nav_icon"></i>
+                        <span class="nav_name">Messages</span>
+                    </a>
+                    <a href="#" class="nav_link">
+                        <i class="bx bx-bookmark nav_icon"></i>
+                        <span class="nav_name">Bookmark</span>
+                    </a>
+                    <a href="#" class="nav_link">
+                        <i class="bx bx-folder nav_icon"></i>
+                        <span class="nav_name">Files</span>
+                    </a>
+                    <a href="#" class="nav_link">
+                        <i class="bx bx-bar-chart-alt-2 nav_icon"></i>
+                        <span class="nav_name">Stats</span>
+                    </a>
+                </div>
+            </div>
+            <a href="#" class="nav_link">
+                <i class="bx bx-log-out nav_icon"></i>
+                <span class="nav_name">SignOut</span>
+            </a>
+        </nav>
+    </div>
 
         <!-- Main content -->
         <main class="height">
