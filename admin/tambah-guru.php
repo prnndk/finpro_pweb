c48a052c4bdef016fa0d2e6a0f18b62ed5791d98
Muhammad Rifqi Ma'ruf <mrifqim.id@gmail.com>
2023-12-14 03:46:19 +0700

    no function

--- admin/tambah-guru.php
+++ admin/tambah-guru.php
@@ -1,22 +1,43 @@
 <?php
 include_once '../connection.php';
 
-function tambahDataGuru($nama_guru, $mapel, $kode, $cabang_id)
-{
-    global $connect;
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    if (
+        isset($_POST['nama_guru']) &&
+        isset($_POST['mapel']) &&
+        isset($_POST['kode']) &&
+        isset($_POST['cabang_id'])
+    ) {
+        $nama_guru = mysqli_real_escape_string($connect, $_POST['nama_guru']);
+        $mapel = mysqli_real_escape_string($connect, $_POST['mapel']);
+        $kode = mysqli_real_escape_string($connect, $_POST['kode']);
+        $cabang_id = mysqli_real_escape_string($connect, $_POST['cabang_id']);
 
-    $nama_guru = mysqli_real_escape_string($connect, $nama_guru);
-    $mapel = mysqli_real_escape_string($connect, $mapel);
-    $kode = mysqli_real_escape_string($connect, $kode);
-    $cabang_id = mysqli_real_escape_string($connect, $cabang_id);
+        $insert_guru_query = "INSERT INTO pengajar (nama, mapel, kode, cabang_id) VALUES ('$nama_guru', '$mapel', '$kode', '$cabang_id')";
+        $insert_guru_result = mysqli_query($connect, $insert_guru_query);
 
-    $insert_guru_query = "INSERT INTO pengajar (nama, mapel, kode, cabang_id) VALUES ('$nama_guru', '$mapel', '$kode', '$cabang_id')";
-    $insert_guru_result = mysqli_query($connect, $insert_guru_query);
-
-    if ($insert_guru_result) {
-        return true;
+        if ($insert_guru_result) {
+            http_response_code(200);
+            $response = ['success' => 'Data guru berhasil ditambahkan'];
+            echo json_encode($response);
+            exit;
+        } else {
+            http_response_code(400);
+            $response = ['error' => 'Gagal menambahkan data guru'];
+            echo json_encode($response);
+            exit;
+        }
     } else {
-        return false;
+        http_response_code(400);
+        $response = ['error' => 'Value not set'];
+        echo json_encode($response);
+        exit;
     }
+} else {
+    http_response_code(400);
+    $response = ['error' => 'Invalid request method'];
+    echo json_encode($response);
+    exit;
 }
 ?>
+
--- admin/tambah_cabang.php
+++ admin/tambah_cabang.php
@@ -1,24 +1,41 @@
 <?php
 include_once '../connection.php';
 
-function tambahDataCabang($nama_cabang, $kota, $alamat, $kontak)
-{
-    global $connect;
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    if (
+        isset($_POST['nama_cabang']) &&
+        isset($_POST['kota']) &&
+        isset($_POST['alamat'])
+    ) {
+        $nama_cabang = mysqli_real_escape_string($connect, $_POST['nama_cabang']);
+        $kota = mysqli_real_escape_string($connect, $_POST['kota']);
+        $alamat = mysqli_real_escape_string($connect, $_POST['alamat']);
+        $kontak = isset($_POST['kontak']) ? mysqli_real_escape_string($connect, $_POST['kontak']) : '';
 
-    $nama_cabang = mysqli_real_escape_string($connect, $nama_cabang);
-    $kota = mysqli_real_escape_string($connect, $kota);
-    $alamat = mysqli_real_escape_string($connect, $alamat);
-    $kontak = mysqli_real_escape_string($connect, $kontak);
+        $insert_cabang_query = "INSERT INTO cabang (nama, kota, alamat, kontak) VALUES ('$nama_cabang', '$kota', '$alamat', '$kontak')";
+        $insert_cabang_result = mysqli_query($connect, $insert_cabang_query);
 
-    $insert_cabang_query = "INSERT INTO cabang (nama, kota, alamat, kontak) VALUES ('$nama_cabang', '$kota', '$alamat', '$kontak')";
-    $insert_cabang_result = mysqli_query($connect, $insert_cabang_query);
-
-    if ($insert_cabang_result) {
-        return true;
+        if ($insert_cabang_result) {
+            http_response_code(200);
+            $response = ['success' => 'Data cabang berhasil ditambahkan'];
+            echo json_encode($response);
+            exit;
+        } else {
+            http_response_code(400);
+            $response = ['error' => 'Gagal menambahkan data cabang'];
+            echo json_encode($response);
+            exit;
+        }
     } else {
-        return false;
+        http_response_code(400);
+        $response = ['error' => 'Value not set'];
+        echo json_encode($response);
+        exit;
     }
+} else {
+    http_response_code(400);
+    $response = ['error' => 'Invalid request method'];
+    echo json_encode($response);
+    exit;
 }
-
 ?>
-
